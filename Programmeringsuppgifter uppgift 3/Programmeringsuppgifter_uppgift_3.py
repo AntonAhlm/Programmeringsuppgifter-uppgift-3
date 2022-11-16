20dbec05326a403753ef353500df0ecbd819b134
Admin <Admin@TEB123012>
2022-11-16 10:19:04 +0100

    Add project files.

---
+++ Programmeringsuppgifter uppgift 3/Programmeringsuppgifter_uppgift_3.py
@@ -0,0 +1,8 @@
+ruta=" "*50
+print(ruta,"+","-"*16,"+")
+print(ruta,"|  Te21C           |")
+print(ruta,"|  Anton Ahlm      |")
+print(ruta,"+","-"*16,"+\n\n")
+
+celsius=int(input("Hur manga grader celsius? "))
+print("Det blir ",(9/5)*celsius+32,"grader fahrenheit.")
